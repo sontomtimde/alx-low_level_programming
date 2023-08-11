b65b444e48fa3cab0dc623c0dae906e531fb4cab
sontomtimde <mtimdesonto@hayoo.com>
2023-08-11 02:19:23 -0700

    5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main -it prints out output using put
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
