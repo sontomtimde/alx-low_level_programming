eac5fbe658b9f9c38896da959e829578ba422e89
sontomtimde <mtimdesonto@hayoo.com>
2023-08-11 01:18:37 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main -it prints out output using put
- *
- *Return: Always(0) 
- */
+* main -it prints out output using put
+*
+* Return: Always 0 (success) 
+*/
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
