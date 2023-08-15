e43fcba21fb89003ebbfcba46d0f87bd747a2fd3
sontomtimde <mtimdesonto@hayoo.com>
2023-08-15 04:32:16 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Make alphabet
+ * return: void
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c ='a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	_putchar('\n');
+}
