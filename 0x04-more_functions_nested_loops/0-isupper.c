3f7b4476ec45a989fd790f1a0c8f2f831df7d819
sontomtimde <mtimdesonto@hayoo.com>
2023-08-20 14:18:03 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper -prog checks for uppercase character
+ * @c: The character to be checked
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
