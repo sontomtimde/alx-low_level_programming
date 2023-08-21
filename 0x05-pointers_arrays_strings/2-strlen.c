0ebe347d8803e1b959f22ea87ca9952e8ec577bc
sontomtimde <mtimdesonto@hayoo.com>
2023-08-21 07:56:49 -0700

    2. This report, by its very length, defends itself against the risk of being read

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -8,9 +8,9 @@
 
 int _strlen(char *s)
 {
-	int i:
+	int i;
 		int count = 0;
-	for (i = 0 ; s[i] != '\0' : i++)
+	for (i = 0 ; s[i] != '\0' ; i++)
 		count++;
-	return (count0);
+	return (count);
 }
