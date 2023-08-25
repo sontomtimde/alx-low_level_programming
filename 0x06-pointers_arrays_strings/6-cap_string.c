54673435df6cdcd4e91d954a1a031f07fe8f60ff
sontomtimde <mtimdesonto@hayoo.com>
2023-08-25 00:53:11 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,6 +3,7 @@
 /**
  * cap_string - Capitalizes all words of a string.
  * @str: The string to be capitalized.
+ *
  * Return: A pointer to the changed string.
  */
 char *cap_string(char *str)
@@ -13,6 +14,7 @@ char *cap_string(char *str)
 	{
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
+		
 		if (str[index - 1] == ' ' ||
 				str[index - 1] == '\t' ||
 				str[index - 1] == '\n' ||
@@ -28,7 +30,9 @@ char *cap_string(char *str)
 				str[index - 1] == '}' ||
 				index == 0)
 			str[index] -= 32;
+		
 		index++;
 	}
+
 	return (str);
 }
