2f296ccb37602523ed7e35f403ab49a16e574af5
sontomtimde <mtimdesonto@hayoo.com>
2023-08-25 07:22:25 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,7 +14,6 @@ char *cap_string(char *str)
 	{
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
-		
 		if (str[index - 1] == ' ' ||
 				str[index - 1] == '\t' ||
 				str[index - 1] == '\n' ||
@@ -30,9 +29,7 @@ char *cap_string(char *str)
 				str[index - 1] == '}' ||
 				index == 0)
 			str[index] -= 32;
-		
 		index++;
 	}
-
 	return (str);
 }
