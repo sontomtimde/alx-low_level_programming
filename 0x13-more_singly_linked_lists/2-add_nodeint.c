0d726046cc09f6833bfcc8f18c7537552c883b61
sontomtimde <mtimdesonto@yahoo.com>
2023-09-26 14:42:58 -0700

    List length

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,18 +1,31 @@
 #include "lists.h"
 
 /**
- * add_nodeint - adds a new node at the beginning of a linked list
- * @head: pointer to the first node in the list
- * @n: data to insert in that new node
+ * add_nodeint - Add a new node to the beginning of a linked list.
+ * @head: A pointer to a pointer to the head of the linked list.
+ * @n: The value to be assigned to the new node.
  *
- * Return: pointer to the new node, or NULL if it fails
+ * This function creates a new node with the specified value and inserts it at
+ * the beginning of a linked list. The 'head' parameter is updated to point to
+ * the newly added node, effectively making it the new head of the list.
+ *
+ * @head: Address of the pointer to the head of the list.
+ * @n: The integer value to store in the new node.
+ *
+ * Return: A pointer to the newly created node, or NULL if malloc fails.
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *new;
+    listint_t *new;
 
-	new = malloc(sizeof(listint_t));
-	if (!new)
-		return (NULL);
-	new->n = n;
+    new = malloc(sizeof(listint_t));
+    if (!new)
+        return (NULL);
+
+    new->n = n;
+    new->next = *head;
+    *head = new;
+
+    return (new);
 }
+
