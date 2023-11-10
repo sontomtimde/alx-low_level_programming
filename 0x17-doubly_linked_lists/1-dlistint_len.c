b9deba821d40d8db0d285bebe5e4d4c05f30dad4
sontomtime <mtimdesonto@yahoo.com>
2023-11-10 12:22:36 -0800

    dlistint_len

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -7,14 +7,14 @@
  **/
 size_t dlistint_len(const dlistint_t *h)
 {
-    const dlistint_t *node = h;
-    size_t count = 0;
+	const dlistint_t *node = h;
+	size_t cont = 0;
 
-    while (node)
-    {
-        count++;
-        node = node->next;
-    }
+	while (node)
+	{
+		cont++;
+		node = node->next;
+	}
 
-    return (count);
+	return (cont);
 }
