dbb8982896250f84ba0f05f42118d46b8d04612b
sontomtime <mtimdesonto@yahoo.com>
2023-11-14 01:18:37 -0800

    Without libraries what have we? We have no past and no future

--- 0x18-dynamic_libraries/9-strcpy.c
+++ 0x18-dynamic_libraries/9-strcpy.c
@@ -24,5 +24,5 @@ char *_strcpy(char *dest, char *src)
 
 	dest[length] = '\0';
 
-	return (*dest);
+	return (dest);
 }
--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,10 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
