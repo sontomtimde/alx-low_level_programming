c3ab45dbf5a34ba467a4da41ce3d49c610831f78
sontomtime <mtimdesonto@yahoo.com>
2023-11-13 23:55:44 -0800

    A library is not a luxury but one of the necessities of life

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-wget -P /usr https://github.com/Viestar/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libhacked.so
-export LD_PRELOAD=/usr/libhacked.so
+wget -P /usr \
+  https://github.com/sontomtimde/alx-low_level_programming/tree/\
+  master/0x18-dynamic_libraries
+export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
